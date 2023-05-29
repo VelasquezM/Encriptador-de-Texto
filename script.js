f61bf9a4de5ddfc17bac747068a8fedfa230e923
Marcos Velásquez <marcosvelasquez7@gmail.com>
2023-05-29 11:55:59 -0400

    Repositorio del Encriptador de texto iniciado, primer commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const btnCopiar = document.querySelector(".btn-copiar");
+
+function btnEncriptar (){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+    btnCopiar.style.display = "block";
+}
+
+function encriptar (stringEncriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptado;
+}
+
+function btnDesencriptar (){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+function desencriptar (stringDesencriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptado;
+}
+
+function copiar () {
+    var contenido = document.querySelector(".mensaje")
+    contenido.select();
+    document.execCommand("cut");
+    alert("¡Texto copiado!");
+}
+
+
